468c91ae9dd01fe0d06f08d539a360602f7faadc
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-08-18 12:55:12 +0700

    chore(index.d.ts): add declarations for image file types to improve type safety and support importing image files in the project

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
 declare interface Window {
   adsbygoogle?: Array<{} | (() => void)>;
 }
+
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpeg";
+declare module "*.jpg";
+declare module "*.webp";
