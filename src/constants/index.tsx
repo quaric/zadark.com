17f80ea0eda6ac36c42adb17a5618835d7df6471
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-08-22 23:53:47 +0700

    feat(constants): update VERSION constant to '14.0' to reflect the latest version of the application

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -38,7 +38,7 @@ export const INSTALL_LINKS_FOR_WEB = [{
   // tutorialLink: '/web/firefox'
 }]
 
-const VERSION = '12.16';
+const VERSION = '14.0';
 
 // export const DOWNLOAD_URLS = {
 //   WINDOWS: `https://zadark.sgp1.digitaloceanspaces.com/windows/ZaDark%20${VERSION}.zip`,
