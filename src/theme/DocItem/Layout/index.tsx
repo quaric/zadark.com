7e012d1863cef5c2da83fe14eb57ba7f5fcf9f0a
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-12-02 21:46:25 +0700

    chore(docusaurus.config.js): remove unused dropdown menu and blog plugin configuration for postal code feature
    build(package.json): update docusaurus dependencies to version 3.6.3 for improved features and bug fixes
    
    chore(postal-code): delete unused postal code related files to clean up the codebase
    
    style(Layout/index.tsx): remove unused import to improve code readability
    
    style(DocItem/Layout/index.tsx): refactor JSX structure for better readability and maintainability

--- src/theme/DocItem/Layout/index.tsx
+++ src/theme/DocItem/Layout/index.tsx
@@ -60,22 +60,23 @@ export default function DocItemLayout({children}: Props): JSX.Element {
           <DocItemPaginator />
         </div>
       </div>
-      {docTOC.desktop && <div className="col col--3">
-        {docTOC.desktop}
-
-        <div className="ads-col-right margin-top--md">
-          <AdsByGoogle>
-            <ins
-              className="adsbygoogle"
-              style={{ display: 'block' }}
-              data-ad-client="ca-pub-5192881442581767"
-              data-ad-slot="6045782918"
-              data-ad-format="auto"
-              data-full-width-responsive="true"
-            ></ins>
-          </AdsByGoogle>
+      {docTOC.desktop && (
+        <div className="col col--3">
+          {docTOC.desktop}
+          <div className="ads-col-right margin-top--md">
+            <AdsByGoogle>
+              <ins
+                className="adsbygoogle"
+                style={{ display: 'block' }}
+                data-ad-client="ca-pub-5192881442581767"
+                data-ad-slot="6045782918"
+                data-ad-format="auto"
+                data-full-width-responsive="true"
+              ></ins>
+            </AdsByGoogle>
+          </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 }
--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Layout from '@theme-original/Layout';
 import type LayoutType from '@theme/Layout';
 import type {WrapperProps} from '@docusaurus/types';
-import BrowserOnly from '@docusaurus/BrowserOnly';
 
 type Props = WrapperProps<typeof LayoutType>;
 
