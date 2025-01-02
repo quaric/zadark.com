9bdd324c549ad127869fb64d5d3f6a43e2162e41
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-01-02 20:37:34 +0700

    chore(changelog): update version to 25.1.1 and adjust PC and Web versions docs(changelog): move previous version details to index and update release date chore(package.json): bump version to 25.1.1 to reflect latest changes fix(constants): update ZADARK_PC_VERSION to 15.1.6 for correct download link

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -38,7 +38,7 @@ export const INSTALL_LINKS_FOR_WEB = [{
   // tutorialLink: '/web/firefox'
 }]
 
-const ZADARK_PC_VERSION = '15.1.5';
+const ZADARK_PC_VERSION = '15.1.6';
 
 export const DOWNLOAD_URLS = {
   WINDOWS: `https://storage.zadark.com/ZaDark%20${ZADARK_PC_VERSION}.zip`,
