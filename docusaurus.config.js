24489d8b32e99998c0d5a3602412edcb76dce1bf
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-11-10 14:20:08 +0700

    feat: add sponsor link to navbar and adjust CSS for improved styling

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -117,6 +117,7 @@ const config = {
           //   className: "header-status-link",
           //   label: "Online",
           // },
+          { href: 'https://github.com/sponsors/ncdai', label: 'Sponsor', position: 'right' },
           {
             href: 'https://github.com/quaric/zadark',
             position: 'right',
